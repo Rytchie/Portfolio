1cbf79260f74ad713500d139db751475014f8149
Rytchie <rytchie1957@gmail.com>
2024-05-13 09:48:12 +0300

    Added RWD

--- pythonProject/WebScrapping/main.py
+++ pythonProject/WebScrapping/main.py
@@ -3,5 +3,4 @@ from bs4 import BeautifulSoup
 with open('practice.html', 'r') as html_file:
     content = html_file.read()
     soup = BeautifulSoup(content, 'lxml')
-    tags = soup.find_all('h1')
-    print(tags)
+    print(soup)
