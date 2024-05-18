f12569785f48bcecb2b9452a9b826e4f7b5e0bb3
Rytchie <rytchie1957@gmail.com>
2024-05-18 16:17:11 +0300

    Long time

--- Web Dev/Test Files/index.js
+++ Web Dev/Test Files/index.js
@@ -1,5 +1,12 @@
-let a = 'Rytchie';
+let fruits = ["apple", "orange", "banana", "coconut"];
+// fruits.push("coconut");
+// fruits.pop();
+// fruits.unshift("mango");
+// fruits.shift();
 
-var a = 'Rytchie';
+for(let i = fruits.length; i >= 0; i--){
+    console.log(fruits[i]);
+}
 
-console.log(a, a);
+
+
